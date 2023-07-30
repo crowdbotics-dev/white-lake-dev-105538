b046748589865ae34d726668d8f8c34d469cfeff
Crowdbotics <engineering@crowdbotics.com>
2023-07-30 23:17:15 +0000

    Initial commit.

---
+++ backend/white_lake_dev_105538/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for white_lake_dev_105538 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'white_lake_dev_105538.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-lake-dev-105538",
+  url: "https://white-lake-dev-105538.botics.co",
+  api: "https://white-lake-dev-105538.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
